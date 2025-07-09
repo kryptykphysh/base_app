cc71bb69b1466e881d729118ab62a7ce599836df
Brian Wherry <brian.wherry@henryscheinone.com>
2025-07-09 08:40:38 +0100

    Associate ChatRoom to ChatMessages

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -2,6 +2,8 @@ class ChatRoom < ApplicationRecord
   validates :title, presence: true, length: { in: 3..50 }, uniqueness: true
   validates :description, presence: true, length: { in: 10..500 }
 
+  has_many :chat_messages, dependent: :destroy
+
   normalizes :title, with: -> (title) { title.strip.gsub(/\s+/, " ").downcase }
 
   broadcasts_refreshes_to ->(chat_room) { chat_room.class.broadcast_target_default }
